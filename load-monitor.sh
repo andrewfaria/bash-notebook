1c488fe877f85a637836b8e5d107a2b693a54c7b
Andrew Mazzarella <github@myimedia.com>
2013-04-26 12:00:43 -0300

    Create load-monitor.sh

---
+++ load-monitor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#config
+max_load="2"
+
+#get current load
+load=`uptime | awk '{print $10}' | tr -d ','`
+
+
+if [ $(echo "$load > $max_load" | bc -l) -eq 1 ]
+then
+    echo "warning"
+fi
